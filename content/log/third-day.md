37581261cb67311167d082c5015838a4fcb19e4e
lqs is a good boy <553912917@qq.com>
2025-03-03 20:03:55 +0800

    初次提交

---
+++ content/log/third-day.md
@@ -0,0 +1,5 @@
++++
+date = '2025-03-03T19:56:16+08:00'
+draft = true
+title = 'Third Day'
++++
---
+++ public/searchindex.en.js
@@ -0,0 +1,60 @@
+var relearn_searchindex = [
+  {
+    "breadcrumb": "My New Hugo Site",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Logs",
+    "uri": "/log/index.html"
+  },
+  {
+    "breadcrumb": "My New Hugo Site \u003e  Posts",
+    "content": "hugo使用 主页：content/_index.md 创建主页：hugo new –kind home _index.md 章节：content/chapter/…………………… 一个章节也是包含一个主页以及子章节 创建章节log及主页：hugo new –kind chapter log/_index.md 在章节log下面创建子章节first-day、second-day hugo new log/first-day/_index.md hugo new log/second-day/index.md hugo new log/third-day.md 2024年6月29日山东省市直遴选笔试题第一题 2024年6月29日山东省市直遴选笔试题第二题",
+    "description": "遴选笔试题（自己做的），老师讲的还没加上",
+    "tags": [
+      "遴选"
+    ],
+    "title": "2024年6月29日山东省市直遴选笔试题",
+    "uri": "/posts/%E7%AC%94%E8%AE%B02024%E5%B9%B46%E6%9C%8829%E6%97%A5%E5%B1%B1%E4%B8%9C%E7%9C%81%E5%B8%82%E7%9B%B4%E9%81%B4%E9%80%89%E7%AC%94%E8%AF%95%E9%A2%98/index.html"
+  },
+  {
+    "breadcrumb": "My New Hugo Site",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Categories",
+    "uri": "/categories/index.html"
+  },
+  {
+    "breadcrumb": "My New Hugo Site",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Posts",
+    "uri": "/posts/index.html"
+  },
+  {
+    "breadcrumb": "My New Hugo Site",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Tags",
+    "uri": "/tags/index.html"
+  },
+  {
+    "breadcrumb": "My New Hugo Site \u003e  Categories",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Category :: 遴选",
+    "uri": "/categories/%E9%81%B4%E9%80%89/index.html"
+  },
+  {
+    "breadcrumb": "My New Hugo Site \u003e  Tags",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Tag :: 遴选",
+    "uri": "/tags/%E9%81%B4%E9%80%89/index.html"
+  }
+]
